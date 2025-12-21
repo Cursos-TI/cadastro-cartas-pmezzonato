bc5a866a73fe77d46b274ce51ba50c0ae78f326d
pmezzonato <pedromezzonato@pm.me>
2025-12-21 12:23:21 +0000

    Primeiro rascunho

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,8 +7,12 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+  int pontos, pop;
+  float area, pib;
+  char estado;
 
+  // Área para entrada de dados
+    
   // Área para exibição dos dados da cidade
 
 return 0;
